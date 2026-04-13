c66fac4d8a30cc438ce10ed938652000b2ab3743
Tobias Gärdebro <tubbeg@gmail.com>
2026-04-13 22:10:35 +0200

    Implemented a few instructions. Need to implement drawing next.

--- emulator.py
+++ emulator.py
@@ -49,16 +49,21 @@ class Memory():
         except:
             return None
 
-
 class Screen():
     def __init__(self) -> None:
         pass
+    def clear(self): return
+    def draw(self, opcode): return
 
 class Emulator():
     def __init__(self) -> None:
         self.memory = None
-        self.screen = {pixel : False for pixel in range(0,16)}
-        self.registers = {"vr":[0 for i in range(0, 16)], "i":0, "pc":0x200}
+        self.screen = Screen()
+        self.registers = {
+            "vr":bytearray(16),
+            "i":0, # index and program counter are represented with ints
+            "pc":0x200 # should actually be 12bit and 16bit
+        }
         self.file_contents = None
         self.run_program = True
     def read_rom(self, path):
@@ -79,39 +84,55 @@ class Emulator():
                 if is_add(b): return Instruction.ADD,opcode
                 if is_seti(b): return Instruction.SETI,opcode
                 if is_draw(b): return Instruction.DRAW,opcode
-        return None
-    def clear_screen(self):
-        for pixel in self.screen:
-            self.screen[pixel] = False
+        return None      
     def set_pc(self, opcode):
         address = 0x0FFF & opcode
         self.registers["pc"] = address
+    def get_register_x(self, opcode):
+        h,_ = opcode
+        return h & 0x0F
+    def get_nn(self, opcode):
+        _,l = opcode
+        return l.to_bytes()
+    def get_nnn(self,opcode):
+        h,l = opcode
+        return bytes_to_word(h,l)
+    def v_add(self, opcode):
+        vreg = self.get_register_x(opcode)
+        add_nn = self.get_nn(opcode)
+        self.registers["vr"][vreg] += add_nn
+    def set_vreg(self,opcode):
+        vreg = self.get_register_x(opcode)
+        set_nn = self.get_nn(opcode)
+        self.registers["vr"][vreg] = set_nn
+    def set_i(self,opcode):
+        set_nnn = self.get_nnn(opcode)
+        self.registers["i"] = set_nnn
     def execute_instruction(self,result):
         if result:
             instruction, opcode = result
             match instruction:
-                case Instruction.CLEAR: return self.clear_screen()
+                case Instruction.CLEAR: return self.screen.clear()
                 case Instruction.JMP: return self.set_pc(opcode)
+                case Instruction.SET: return self.set_vreg(opcode)
+                case Instruction.ADD: return self.v_add(opcode)
+                case Instruction.SETI: return self.set_i(opcode)
+                case Instruction.DRAW: return self.screen.draw(opcode)
                 case _: return
         return
     def terminate(self):
         while self.run_program:
-            i = input("Abort? (Y)") 
-            if i == "Y":
+            if input("Abort? (Y)") == "Y":
                 self.run_program = False
     def run_cpu_loop(self):
         if self.memory:
             t = threading.Thread(target=self.terminate)
-            print("alive?",t.is_alive())
             t.start()
-            print("alive?",t.is_alive())
             while self.run_program:
                 instruction = self.get_instruction() # decode opcode/fetch instruction
                 self.registers["pc"] = self.registers["pc"] + 2 # increment pc by two (each instruction is 16-bit)
                 self.execute_instruction(instruction) # execute instruction
-            print("alive?",t.is_alive())
             t.join()
-            print("alive?",t.is_alive())
 
 
 if __name__ == "__main__":
